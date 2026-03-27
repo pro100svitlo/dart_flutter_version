82a19e8d4d891275308af64274f5b44ecd432a39
Andrii Antonov <pro100svitlo@gmail.com>
2026-03-27 00:17:42 +0000

    feat: Update dart_to_flutter_map.dart file with new versions
    Dart 3.11.4 -> Flutter 3.41.6

--- lib/src/dart_to_flutter_map.dart
+++ lib/src/dart_to_flutter_map.dart
@@ -12,6 +12,7 @@ import 'package:pub_semver/pub_semver.dart';
 /// To find ouf more about the generation process, check README.md
 /// file in the root directory.
 final dartToFlutterMap = {
+  Version.parse('3.11.4'): Version.parse('3.41.6'),
   Version.parse('3.11.3'): Version.parse('3.41.5'),
   Version.parse('3.11.1'): Version.parse('3.41.4'),
   Version.parse('3.11.0'): Version.parse('3.41.2'),
